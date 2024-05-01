f3c0da0c421672705d66a978ded5f51bb855938e
Sudev Nair <sudevnair@192.168.1.6>
2024-05-01 11:07:03 +0530

    correctly implemented back propagation

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,39 @@
-use std::borrow::BorrowMut;
+use std::cell::RefCell;
 use std::fmt;
 use std::ops::{Add, Mul};
+use std::rc::Rc;
 
 #[derive(Debug)]
-enum TValue<'a> {
-    Flt(f64),
-    Norm(Value<'a>),
-    RNorm(&'a Value<'a>),
-}
-
-#[derive(Debug)]
-struct Value<'a> {
+struct Value {
+    label: String,
     data: f64,
     grad: f64,
-    // _backward: Box<dyn FnMut()>,
-    _prev: Option<Vec<TValue<'a>>>,
+    _backward: Option<Vec<f64>>,
+    _prev: Option<Vec<Rc<RefCell<Value>>>>,
     _op: Option<String>,
 }
 
-impl<'a> Value<'a> {
-    fn new(data: f64) -> Self {
+impl<'a> Value {
+    fn new(data: f64, label: String) -> Self {
         let value = Value {
+            label,
             data,
             grad: 0.0000,
+            _backward: None,
             _prev: None,
             _op: None,
         };
         value
     }
 }
-//Formating
-impl<'a> fmt::Display for TValue<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            TValue::Norm(data) => write!(f, "({}", data),
-            TValue::RNorm(data) => write!(f, "({}", data),
-            TValue::Flt(data) => write!(f, "(Value: {})", data),
-        }
-    }
-}
-impl<'a> fmt::Display for Value<'a> {
+
+impl fmt::Display for Value {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "(Value: {:.4}, grad: {}, Prev: ", self.data, self.grad)?;
         if let Some(prev) = &self._prev {
             for (index, item) in prev.iter().enumerate() {
-                write!(f, "{},", item)?;
+                let item_rc = item.borrow();
+                write!(f, "{},", item_rc)?;
                 if index < prev.len() - 1 {
                     write!(f, "->")?;
                 }
@@ -61,192 +50,122 @@ impl<'a> fmt::Display for Value<'a> {
         Ok(())
     } // Displaying value with two decimal places
 }
+
 //Add
-impl<'a, T> Add<T> for Value<'a>
+impl<T> Add<T> for Value
 where
     T: Into<Option<f64>>,
 {
     type Output = Self;
-    fn add(self, other: T) -> Self::Output {
-        let other_data = other.into().unwrap_or(0.0);
+    fn add(mut self, other: T) -> Self::Output {
+        let mut other_data = Value::new(other.into().unwrap_or(0.0), "const".to_string());
+        self._op = Some("+".to_string());
+        other_data._op = Some("*".to_string());
         Self {
-            data: self.data + other_data,
+            label: format!("{}+{}", self.label, other_data.label),
+            data: self.data + other_data.data,
             grad: 0.0000,
-            _prev: Some(vec![TValue::Norm(self), TValue::Flt(other_data)]),
-            _op: Some("+".to_string()),
-        }
-    }
-}
-impl<'a> Add<&'a Value<'a>> for Value<'a> {
-    type Output = Self;
-    fn add(self, other: &'a Value<'a>) -> Self::Output {
-        Self {
-            data: self.data + other.data,
-            grad: 0.0000,
-            _prev: Some(vec![TValue::Norm(self), TValue::RNorm(other)]),
-            _op: Some("+".to_string()),
-        }
-    }
-}
-impl<'a> Add for Value<'a> {
-    type Output = Value<'a>;
-    fn add(self, other: Self) -> Self::Output {
-        Value {
-            data: self.data + other.data,
-            grad: 0.0000,
-            _prev: Some(vec![TValue::Norm(self), TValue::Norm(other)]),
-            _op: Some("+".to_string()),
+            _backward: Some(vec![self.data, other_data.data]),
+            _prev: Some(vec![
+                Rc::new(RefCell::new(self)),
+                Rc::new(RefCell::new(other_data)),
+            ]),
+            _op: None,
         }
     }
 }
 
-impl<'a> Add for &'a Value<'a> {
-    type Output = Value<'a>;
-    fn add(self, other: Self) -> Self::Output {
+impl Add for Value {
+    type Output = Value;
+    fn add(mut self, mut other: Self) -> Self::Output {
+        self._op = Some("+".to_string());
+        other._op = Some("+".to_string());
         Value {
+            label: format!("{}+{}", self.label, other.label),
             data: self.data + other.data,
             grad: 0.0000,
-            _prev: Some(vec![TValue::RNorm(self), TValue::RNorm(other)]),
-            _op: Some("+".to_string()),
-        }
-    }
-}
-impl<'a> Add<Value<'a>> for &'a Value<'a> {
-    type Output = Value<'a>;
-    fn add(self, other: Value<'a>) -> Self::Output {
-        Value {
-            data: self.data + other.data,
-            grad: 0.0000,
-            _prev: Some(vec![TValue::RNorm(self), TValue::Norm(other)]),
-            _op: Some("+".to_string()),
-        }
-    }
-}
-impl<'a, T> Add<T> for &'a Value<'a>
-where
-    T: Into<f64>,
-{
-    type Output = Value<'a>;
-    fn add(self, other: T) -> Self::Output {
-        let o = other.into();
-        Value {
-            data: self.data + &o,
-            grad: 0.0000,
-            _prev: Some(vec![TValue::RNorm(self), TValue::Flt(o)]),
-            _op: Some("+".to_string()),
+            _backward: Some(vec![self.data, other.data]),
+            _prev: Some(vec![
+                Rc::new(RefCell::new(self)),
+                Rc::new(RefCell::new(other)),
+            ]),
+            _op: None,
         }
     }
 }
 
 //Implementing multiply
-// impl<T> Mul<T> for Value
-// where
-//     T: Into<f64>,
-// {
-//     type Output = Self;
-//     fn mul(self, other: T) -> Self::Output {
-//         Self {
-//             data: self.data * other.into(),
-//             _prev: Some(vec![TValue::Norm(self), TValue::Flt(other.into())]),
-//         }
-//     }
-// }
-// impl Mul<&Value> for Value {
-//     type Output = Self;
-//     fn mul(self, other: &Value) -> Self::Output {
-//         Self {
-//             data: self.data * other.data,
-//             _prev: vec![self.data, other.data],
-//         }
-//     }
-// }
-// impl Mul for Value {
-//     type Output = Value;
-//     fn mul(self, other: Self) -> Self::Output {
-//         Value {
-//             data: self.data * other.data,
-//             _prev: vec![self.data, other.data],
-//         }
-//     }
-// }
+impl<T> Mul<T> for Value
+where
+    T: Into<Option<f64>>,
+{
+    type Output = Self;
+    fn mul(mut self, other: T) -> Self::Output {
+        let mut other_data = Value::new(other.into().unwrap_or(0.0), "const".to_string());
+        self._op = Some("*".to_string());
+        other_data._op = Some("*".to_string());
+        Self {
+            label: format!("{}*{}", self.label, other_data.label),
+            data: self.data * other_data.data,
+            grad: 0.0000,
+            _backward: Some(vec![self.data, other_data.data]),
+            _prev: Some(vec![
+                Rc::new(RefCell::new(self)),
+                Rc::new(RefCell::new(other_data)),
+            ]),
+            _op: None,
+        }
+    }
+}
 
-// impl Mul for &Value {
-//     type Output = Value;
-//     fn mul(self, other: Self) -> Self::Output {
-//         Value {
-//             data: self.data * other.data,
-//             _prev: vec![self.data, other.data],
-//         }
-//     }
-// }
-// impl Mul<Value> for &Value {
-//     type Output = Value;
-//     fn mul(self, other: Value) -> Self::Output {
-//         Value {
-//             data: self.data * other.data,
-//             _prev: vec![self.data, other.data],
-//         }
-//     }
-// }
-// impl<T> Mul<T> for &Value
-// where
-//     T: Into<f64>,
-// {
-//     type Output = Value;
-//     fn mul(self, other: T) -> Self::Output {
-//         Value {
-//             data: self.data * other.into(),
-//             _prev: vec![self.data, other.into()],
-//         }
-//     }
-// }
-//     T: Into<f64>,
-// {
-//     type Output = Value;
-//     fn add(self, other: T) -> Self::Output {
-//         Value {
-//             data: self.data + other.into(),
-//             _prev: Some(vec![TValue::RNorm(self), TValue::Flt(other.into())]),
-//         }
-//     }
-// }
+impl Mul for Value {
+    type Output = Value;
+    fn mul(mut self, mut other: Self) -> Self::Output {
+        self._op = Some("*".to_string());
+        other._op = Some("*".to_string());
+        Value {
+            label: format!("{}*{}", self.label, other.label),
+            data: self.data * other.data,
+            grad: 0.0000,
+            _backward: Some(vec![self.data, other.data]),
+            _prev: Some(vec![
+                Rc::new(RefCell::new(self)),
+                Rc::new(RefCell::new(other)),
+            ]),
+            _op: None,
+        }
+    }
+}
 
 fn indent(level: usize) -> String {
     const INDENT: &str = "    ";
     INDENT.repeat(level)
 }
 
-fn generate_chart<'a>(value: &'a Value<'a>, level: usize) -> String {
+fn generate_chart<'a>(value: &Value, level: usize) -> String {
     let mut result = String::new();
     if let Some(ope) = &value._op {
         result.push_str(&format!(
-            "{}├─{}(Value: {}, grad: {})\n",
+            "{}├─{}({}||Value: {}, grad: {})\n",
             indent(level),
             ope,
+            value.label,
             value.data,
             value.grad
         ));
     } else {
         result.push_str(&format!(
-            "{}├─=(Value: {}, grad: {})\n",
+            "{}├─=({}||Value: {}, grad: {})\n",
             indent(level),
+            value.label,
             value.data,
             value.grad
         ));
     }
     if let Some(prev) = &value._prev {
         for item in prev {
-            match item {
-                TValue::Norm(sub_value) => {
-                    result.push_str(&generate_chart(sub_value, level + 1));
-                }
-                TValue::RNorm(rsub_value) => {
-                    result.push_str(&generate_chart(rsub_value, level + 1));
-                }
-                TValue::Flt(data) => {
-                    result.push_str(&format!("{}├─+(Value: {})\n", indent(level + 1), data));
-                }
-            }
+            let item_rc = item.borrow();
+            result.push_str(&generate_chart(&*item_rc, level + 1));
         }
     }
     result
@@ -255,27 +174,41 @@ fn generate_chart<'a>(value: &'a Value<'a>, level: usize) -> String {
 //Backpropagation
 fn propagate(value: &mut Value) {
     value.grad = 1.0000;
-    if let Some(prev) = &value._prev {
-        for i in prev.iter() {
-            print!("{}", i)
+    backward(value);
+    fn backward(bvalue: &mut Value) {
+        if let Some(prev) = &bvalue._prev {
+            let list = prev.iter();
+            let mut i: usize = 0;
+            for temp in list {
+                let mut i_rc = temp.borrow_mut();
+                //let mut u_rc = u.borrow_mut();
+                if let Some(ope) = &i_rc._op {
+                    match ope.as_str() {
+                        "+" => i_rc.grad = bvalue.grad,
+                        "*" => {
+                            if let Some(backward) = &bvalue._backward {
+                                i = i + 1;
+                                if let Some(last) = backward.iter().nth(i % 2) {
+                                    i_rc.grad += last * bvalue.grad;
+                                }
+                            }
+                        }
+                        _ => i_rc.grad = 0.0000,
+                    }
+                } else {
+                    i_rc.grad = 1.0000;
+                }
+                backward(&mut *i_rc)
+            }
         }
     }
-    print!("No previous");
-}
-
-fn f(x: f64) -> f64 {
-    3.00 * f64::powf(x, 2.00) - 4.00 * x + 5.00
 }
 
 fn main() {
-    let mut a = Value::new(2.00);
-    let mut b = Value::new(-3.00);
-    let mut c = Value::new(10.00);
-    let mut d = &a + c + &b + 1.00;
-    // let result = f(x + h);
-    // let num = f(x);
-    // let diff = (result - num) / h;
-    // println!("a: {} b: {} d(a+b): {}", a, b, d)
-    //propagate(&mut d);
+    let a = Value::new(2.00, 'a'.to_string());
+    let b = Value::new(-3.00, 'b'.to_string());
+    let c = Value::new(10.00, 'c'.to_string());
+    let mut d = a + c * b * 2.00 * 1.00;
+    propagate(&mut d);
     println!("{}", generate_chart(&d, 1));
 }
